df4c83da1b9b6e630619a21f4ece461e66d5c474
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-30 17:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function firstWord(s) {
   // your code here
-	if(s.length<=0){
-		return '';
-	}
-	let str = s.trim();
-let word = "";
-
-for (let i = 0; i < str.length; i++) {
-  if (str.charAt(i) == " ") {
-    return str.substring(0, i)
+  if(s.length <= 0){
+    return '';
   }
-}
+  let str = s.trim();
+  let word = "";
 
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) == " ") {
+      return str.substring(0, i)
+    }
+  }
+  
+  return str; // return the entire string if no spaces were found
 }
 
 // Do not change the code below
