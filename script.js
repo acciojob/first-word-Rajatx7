44d4eb46d68a16fac5965c98a2936a9a999b0df1
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-30 17:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
   // your code here
+	if(s.length<=0){
+		return '';
+	}
+	let str = s.trim();
+let word = "";
+
+for (let i = 0; i < str.length; i++) {
+  if (str.charAt(i) == " ") {
+    return str.substring(0, i)
+  }
+}
+
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
